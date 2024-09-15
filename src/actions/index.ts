2b4d2aed63d7977bf917c3c9c964de09ce92beb0
Caroline Courtney <caroline@carolinecourtney.com>
2024-09-15 13:30:15 -0400

    error page for uncaught errors; but better way is to catch them

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -17,18 +17,26 @@ export async function deleteSnippet(id: number) {
   });
   redirect('/');
 }
-
+[]
 export async function createSnippet(formState: {message: string}, formData: FormData) {
-  const title = formData.get('title');
-  const code = formData.get('code');
-  if (typeof title !== 'string' || title.length < 3) {
-    return {message: 'title must be longer'}
+  try {
+    const title = formData.get('title');
+    const code = formData.get('code');
+    if (typeof title !== 'string' || title.length < 3) {
+      return {message: 'title must be longer'}
+    }
+    if (typeof code !== 'string' || code.length < 10) {
+      return {message: 'code must be longer'}
+    }
+    await db.snippet.create({
+      data: { title, code }
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return { message: err.message}
+    } else {
+      return { message: 'something went wrong'}
+    }
   }
-  if (typeof code !== 'string' || code.length < 10) {
-    return {message: 'code must be longer'}
-  }
-  const snippet = await db.snippet.create({
-    data: { title, code }
-  });
   redirect('/');
 }
